bf6a7a8bbfc63ade4f006c23878c38b8ddf259ca
Pintos Joaquin <joale7546@gmail.com>
2024-07-03 20:19:11 -0300

    Inicializacion del repo

---
+++ src/main/java/com/codingdojo/joaquin/RepositorioColaborativoApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.joaquin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositorioColaborativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositorioColaborativoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingdojo/joaquin/RepositorioColaborativoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.joaquin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositorioColaborativoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
